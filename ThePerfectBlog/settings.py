e86b39d510f300ba7ce3b4f175d5ca9a07227e65
Tom Roy <tbr9ts@virginia.edu>
2016-02-18 02:05:31 -0500

    committing

--- ThePerfectBlog/settings.py
+++ ThePerfectBlog/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'ThePerfectBlog.urls'
 TEMPLATES = (
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["blog","blog/templates/blog"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ThePerfectBlog/urls.py
+++ ThePerfectBlog/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+import blog.views as views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ThePerfectBlog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home),
+    url(r'^accounts/login/$', auth_views.login),
+    url(r'^accounts/create/$', views.createuser),
+    url(r'^(\d+)$', views.viewpost),
+    url(r'^new$', views.new),
+    url(r'^account$', views.account),
+    url(r'^accounts/delete', views.delete),
 ]
